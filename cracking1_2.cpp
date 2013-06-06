243b432e9b3e4ce7619d6f31065bde24e266a897
daianqi <daianqi@umich.edu>
2013-06-05 23:08:21 -0400

    1-2

---
+++ cracking1_2.cpp
@@ -0,0 +1,19 @@
+/*
+Implement a function void reverse(char* str) in C or C++ which reverse a null-terminated string.
+*/
+void reverse(char* str) {
+    char* end = str;
+    char temp;
+
+    if(str){
+        while(*end)
+            end++;
+        end--;
+        while(str < end){
+            temp = *str;
+            *str++ = *end;
+            *end-- = temp;
+        }
+    }
+}
+
