d973565fbe9f6e28eec0fc82278a03df0fa7db10
daianqi <daianqi@umich.edu>
2013-06-16 15:30:21 -0400

    4-6

--- TreeNode.java
+++ TreeNode.java
@@ -1,5 +1,6 @@
 public class TreeNode {
     public int value;
+    public TreeNode parent;
     public TreeNode left;
     public TreeNode right;
 
@@ -7,12 +8,21 @@ public class TreeNode {
         left = null;
         right = null;
         value = -1;
+        parent = null;
     }
 
     public TreeNode(int i) {
         value = i;
         left = null;
         right = null;
+        parent = null;
+    }
+
+    public TreeNode(int value, TreeNode parent) {
+        this.value = value;
+        left = null;
+        right = null;
+        this.parent = parent;
     }
 
 //    public TreeNode(int i, int left, int right) {
@@ -30,6 +40,19 @@ public class TreeNode {
             this.right = null;
         else
             this.right = new TreeNode(right);
+    }
+
+    public void addChildWithParent(int left,int right) {
+        if(left == -1)
+            this.left = null;
+        else {
+            this.left = new TreeNode(left, this);
+        }
+        if(right == -1)
+            this.right = null;
+        else {
+            this.right = new TreeNode(right,this);
+        }
     }
 
 }
---
+++ cracking4_6.java
@@ -0,0 +1,58 @@
+/*
+Write an algorithm to find the 'next' node (in-order successor) of a given node in a binary search tree. You may assume that
+each node has a link to it parent.
+*/
+
+import java.lang.System;
+
+public class cracking4_6 {
+
+    public static TreeNode leftMost(TreeNode root) {
+        if(root == null)
+            return null;
+        while(root.left != null)
+            root = root.left;
+        return root;
+    }
+
+    public static TreeNode getNext(TreeNode n) {
+        if(n == null)
+            return null;
+        if(n.right != null)
+            return leftMost(n.right);
+        else {
+//            TreeNode c = n;
+//            TreeNode p = c.parent;    //stupid kid: you need to keep child and parent sperately
+            TreeNode p = n;
+//            while(p != null && p.left != c) {
+//                c = p;
+//                p = p.parent;
+//            }
+            while(p.parent != null && p.parent.left != p) {
+                p = p.parent;
+            }
+//            return p;
+            return p.parent; //when I return to the root and it's still not a left child, I will return null!
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.addChildWithParent(2, 8);
+        root.left.addChildWithParent(1, 3);
+        root.right.addChildWithParent(6, 9);
+        root.left.left.addChildWithParent(-1, -1);
+        root.left.right.addChildWithParent(-1, 4);
+        root.right.left.addChildWithParent(-1, 7);
+        root.right.right.addChildWithParent(-1, 10);
+
+        System.out.println(getNext(root).value);
+        System.out.println(getNext(root.left.right.right).value);
+//        System.out.println(getNexr(root.right.right.right).value); //I fix this bug for a night, FUUUUUUCK
+        TreeNode result = getNext(root.right.right.right);
+        if(result == null)
+            System.out.println("NULL");
+        else
+            System.out.println(result.value);
+    }
+}
