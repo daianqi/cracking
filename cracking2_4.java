eeafafb9d14ce92ace1bc8b2753ac2f8a5c6d5f3
godstears <daianqi@umich.edu>
2013-06-18 22:38:24 -0300

    Update cracking2_4.java

--- cracking2_4.java
+++ cracking2_4.java
@@ -1,6 +1,5 @@
 /*
-Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than
-or equal to x.
+Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
 */
 
 import java.lang.Math;
