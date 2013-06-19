676870144c7704d6fb887345f06dbd0cd21657fe
godstears <daianqi@umich.edu>
2013-06-18 22:34:59 -0300

    Update cracking4_9.java

--- cracking4_9.java
+++ cracking4_9.java
@@ -1,6 +1,5 @@
 /*
-You are given a binary tree in which each node contains a value. Design an algorithm to print all paths which sum to a given
-value. The path does not need to start or end at the root or a leaf.
+You are given a binary tree in which each node contains a value. Design an algorithm to print all paths which sum to a given value. The path does not need to start or end at the root or a leaf.
 */
 
 import java.lang.Integer;
