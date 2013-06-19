31ed25d8f0d5c9cbf5522c6740a9387f9f991c69
godstears <daianqi@umich.edu>
2013-06-18 22:36:22 -0300

    Update cracking4_3.java

--- cracking4_3.java
+++ cracking4_3.java
@@ -1,6 +1,5 @@
 /*
-Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with
-minimal height.
+Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
 */
 
 import java.lang.Integer;
