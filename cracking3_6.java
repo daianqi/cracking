3f757a700ef8398afeab2586d9745c6f86a0145e
godstears <daianqi@umich.edu>
2013-06-18 22:37:05 -0300

    Update cracking3_6.java

--- cracking3_6.java
+++ cracking3_6.java
@@ -1,7 +1,5 @@
 /*
-Write a program to sort a stack in ascending order (with biggest items on top). You may use at most one additional stack to
-hold items, but you may not copy the elements into any other data structure (such as an array). The stack supports the following
-operations: push, pop, peek and isEmpty.
+Write a program to sort a stack in ascending order (with biggest items on top). You may use at most one additional stack to hold items, but you may not copy the elements into any other data structure (such as an array). The stack supports the following operations: push, pop, peek and isEmpty.
 */
 
 import java.lang.Integer;
