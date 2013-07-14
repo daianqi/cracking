08627df40a9b18515556d3c6b2c67f829ad3cb00
godstears <daianqi@umich.edu>
2013-07-14 15:46:28 -0400

    input:node  output:complete copy of input structure

--- cracking13_7.cpp
+++ cracking13_7.cpp
@@ -1,6 +1,6 @@
 /*
-Write a method that takes a pointer to a Node structure as a parameter and returns a complete copy of the passed in data
-structure. The Node data structure contains two pointers to other Nodes.
+Write a method that takes a pointer to a Node structure as a parameter and returns a complete copy of the passed in data structure. 
+The Node data structure contains two pointers to other Nodes.
 */
 
 typedef map<Node*, Node*> NodeMap;
