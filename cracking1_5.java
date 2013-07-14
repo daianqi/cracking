ce479481ba9e866ed89ae619a6ad65b6f0f00aad
godstears <daianqi@umich.edu>
2013-07-14 15:59:14 -0400

    string compression using the counts of repeated characters

--- cracking1_5.java
+++ cracking1_5.java
@@ -3,6 +3,7 @@ Implement a method to perform basic string compression using the counts of repea
 would become a2b1c5a3. If the "compressed" string would not become smaller than the original string, your method should return
 the original string.
 */
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
